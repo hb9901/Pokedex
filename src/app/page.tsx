953b428829a3eada5c2a47796529663c7aa9e60b
hb9901 <hb9901@gmail.com>
2024-07-02 09:33:29 +0900

    fix: 포켓몬 데이터 호출 위치 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
+"use client";
+
+import Loading from "@/components/Loading";
 import PokemonList from "@/components/PokemonList";
+import { TPokemon } from "@/schemas/pokemon.type";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [isInitialized, setIsInitialized] = useState<boolean>(false);
+  const [pokemons, setPokemons] = useState<TPokemon[] | null>(null);
+
+  useEffect(() => {
+    const getPokemonList = async () => {
+      const response = await axios.get<Promise<TPokemon[]>>(
+        "http://localhost:3000/pokemons"
+      );
+      const pokemonsData = await response.data;
+      setPokemons(pokemonsData);
+      setIsInitialized(true);
+    };
+    getPokemonList();
+  }, []);
+
   return (
-    <main className="max-w-[1800px] m-auto">
-      <PokemonList />
+    <main className="flex flex-col items-center max-w-[1800px] m-auto">
+      <h1>포켓몬 도감</h1>
+      {isInitialized ? <PokemonList pokemons={pokemons} /> : <Loading />}
     </main>
   );
 }
--- src/components/PokemonList/PokemonList.tsx
+++ src/components/PokemonList/PokemonList.tsx
@@ -1,37 +1,17 @@
-"use client";
-
 import { TPokemon } from "@/schemas/pokemon.type";
-import axios from "axios";
-import { useEffect, useState } from "react";
-import Loading from "../Loading";
 import PokemonListCard from "../PokemonListCard";
 
-function PokemonList() {
-  const [isInitialized, setIsInitialized] = useState<boolean>(false);
-  const [pokemons, setPokemons] = useState<TPokemon[] | null>(null);
+interface PokemonListProps {
+  pokemons: TPokemon[] | null;
+}
 
-  useEffect(() => {
-    const getPokemonList = async () => {
-      const response = await axios.get<Promise<TPokemon[]>>(
-        "http://localhost:3000/pokemons"
-      );
-      const pokemonsData = await response.data;
-      setPokemons(pokemonsData);
-      setIsInitialized(true);
-    };
-    getPokemonList();
-  }, []);
-
+function PokemonList({ pokemons }: PokemonListProps) {
   return (
     <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-x-3 gap-y-8">
-      {isInitialized ? (
-        pokemons &&
+      {pokemons &&
         pokemons.map((pokemon) => (
           <PokemonListCard key={pokemon.id} pokemon={pokemon} />
-        ))
-      ) : (
-        <Loading />
-      )}
+        ))}
     </div>
   );
 }
